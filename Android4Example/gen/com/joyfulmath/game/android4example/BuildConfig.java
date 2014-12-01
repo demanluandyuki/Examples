564fb1c39008609a6df692c870ae3c9c41deaa64
deman_lu <deman_lu@htc.com>
2014-12-01 16:48:39 +0800

    [deman_lu]add andrid4example Symptom: Root Cause: Solution: Project:PromoterWeb Note:N/A

---
+++ Android4Example/gen/com/joyfulmath/game/android4example/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.joyfulmath.game.android4example;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Android4Example/src/com/joyfulmath/game/android4example/MainActivity.java
@@ -0,0 +1,29 @@
+package com.joyfulmath.game.android4example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);		
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+	
+	
+}
