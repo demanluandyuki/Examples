141e66e3b0509573353e68f9566aae7907d0bfb5
luyc1 <luyc1@lenovo.com>
2015-09-11 15:18:20 +0800

    modify field

--- JavaStudy/src/com/joyfulmath/study/field/Person.java
+++ JavaStudy/src/com/joyfulmath/study/field/Person.java
@@ -1,8 +1,15 @@
 package com.joyfulmath.study.field;
 
+import com.joyfulmath.study.utils.TraceLog;
+
 public class Person {
 	public String name;
 
+	public Person()
+	{
+		TraceLog.v("Person construct");
+	}
+	
 	public String getName() {
 		return name;
 	}
--- JavaStudy/src/com/joyfulmath/study/field/Student.java
+++ JavaStudy/src/com/joyfulmath/study/field/Student.java
@@ -1,13 +1,16 @@
 package com.joyfulmath.study.field;
 
+import com.joyfulmath.study.utils.TraceLog;
+
 public class Student extends Person {
 	
 	public String name;
-	
+	public int level;
 	public Student(String name)
 	{
 		this.name = name;
 		super.name = "Man-Person";
+		TraceLog.v("Student construct");
 	}
 	
 	@Override
